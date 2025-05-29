fff8a42b57333832557788ad4f8a2fc586494bfb
Thiago Campos <commit@thigcampos.com>
2025-05-29 10:54:50 -0300

    include port and source options

--- Sources/Cubo.swift
+++ Sources/Cubo.swift
@@ -1,14 +1,24 @@
+import ArgumentParser
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-// 
+//
 // Swift Argument Parser
 // https://swiftpackageindex.com/apple/swift-argument-parser/documentation
-
-import ArgumentParser
+import Foundation
 
 @main
 struct Cubo: ParsableCommand {
+    static let config = CommandConfiguration(
+        commandName: "cubo",
+        abstract: "A simple HTTP server application inspired by Python's HTTP.server"
+    )
+
+    @Option(name: .shortAndLong, help: "The port to listen on.")
+    var port: Int = 8080
+
+    @Option(name: .shortAndLong, help: "The directory to serve files from.")
+    var source: String = FileManager.default.currentDirectoryPath
+
     mutating func run() throws {
-        print("Hello, world!")
     }
 }
